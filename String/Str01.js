668735abb43add9dc10699a2b3412b4aa87faf09
Pisodev77 <pisodev0427@gmail.com>
2024-01-18 10:55:34 +0900

    feat: 문자열을 쓸 수 있는 경우들 \

---
+++ String/Str01.js
@@ -0,0 +1,26 @@
+// 따옴표
+const str1 = 'Single Quote';
+
+// 쌍따옴표
+//prettier-ignore
+const str2 = "Double Quote";
+
+// 역따옴표 | Template literals | 백틱
+const str3 = `Template literals or Backtick`;
+
+// String() 생성자
+const str4 = String(212);
+// === '212'
+
+// toString() 메서드
+const num = 212;
+const str5 = num.toString();
+
+// 문자열 연결 concatenation '+' 연산자
+const str6 = 'con' + 'cat';
+
+// 템플릿 리터럴 내에 변수를 포함
+const str7 = `The number is ${num}`;
+
+// String.fromCharCode()
+const str8 = String.fromCharCode(72, 101, 108, 108);
