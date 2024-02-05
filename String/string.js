79ac3ebf1b68cf94305e14ec012f4ed8c9dadc6f
Pisodev77 <pisodev0427@gmail.com>
2024-02-05 13:49:56 +0900

    feat: String 연습 4번째 와 5번째

--- String/string.js
+++ String/string.js
@@ -3,6 +3,8 @@ const $inputEle = document.getElementById('inputEle');
 const str01Result = document.querySelector('.str01 > .result');
 const str02Result = document.querySelector('.str02 > .result');
 const str03Result = document.querySelector('.str03 > .result');
+const str04Result = document.querySelector('.str04 > .result');
+const str05Result = document.querySelector('.str05 > .result');
 
 const exs = [
 	/**
@@ -32,14 +34,33 @@ const exs = [
 	 * - html,css,js의 다른 부분은 걷들지 말아주세요.
 	 */
 	(str) => str.toLowerCase(),
+	/**
+	 * 문자열의 길이를 반환하는 함수
+	 * @param {string} str
+	 * @returns
+	 */
+	(str) => {
+		// Do it!
+	},
+	/**
+	 * 입력받은 문자 A를 a로 바꾸는 함수
+	 * @param {string} str
+	 */
+	() => {
+		const str = 'A B C D A A B N D D C C E E';
+	},
 ];
 
 $inputEle.addEventListener('change', (e) => {
-	const { str01: str01Input, str03: str03Input } = e.currentTarget;
+	const { str01: str01Input, str03: str03Input, str04: str04Input } = e.currentTarget;
 
 	str01Result.textContent = exs[0](str01Input.value);
 
 	str02Result.textContent = exs[1]();
 
 	str03Result.textContent = exs[2](str03Input.value);
+
+	str04Result.textContent = exs[3](str04Input.value);
+
+	str05Result.textContent = exs[4]();
 });
